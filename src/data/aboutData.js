731d619b9feb459b9e247e2c74322f8299042f9f
misathemeb <melissabridi@gmail.com>
2022-08-27 10:17:42 -0400

    updated about

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -2,6 +2,6 @@ export const aboutData = {
     title: "Hello, I'm Melissa!",
     description1: "I'm an eternally curious research scientist and front-end developer based in Pittsburgh. I have 2 B.Sc degrees (Athletic Training & Biology) and am a self-taught programmer through Codecademy Pro's front-end engineer career path.",
     description2: "After 10 years working in clinical research, I decided to merge my passions and set out to equip myself with the tools needed to work in health tech. It's not often that an end-user of materials developed for use in medicine switches roles, but I love a challenge!",
-    description3: "I started with Python since collecting and analyzing data was a natural transition from my technical background.  It's been exciting to learn in detail how the web works, and gain exposure to all the interesting tooling that comes out every five seconds in tech. I can't wait to contribute to solving problems and make SaaS products easy for everyone to use!",
+    description3: "Currently, I work with Python in Product Operations. I love being able to contribute to solving problems and make comprehensive SaaS products easier to use!",
     image: 1
 }
