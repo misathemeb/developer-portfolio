5fe4031ca0a7629f50649c32813d99b538d9a382
misathemeb <melissabridi@gmail.com>
2023-01-06 17:52:38 -0500

    updated desc3 to reflect responsibilities

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -2,6 +2,6 @@ export const aboutData = {
     title: "Hello, I'm Melissa!",
     description1: "I'm an eternally curious research scientist and front-end developer based in Pittsburgh. I have 2 B.Sc degrees (Athletic Training & Biology) and am a self-taught programmer through Codecademy Pro's front-end engineer career path.",
     description2: "After 10 years working in clinical research, I decided to merge my passions and set out to equip myself with the tools needed to work in health tech. It's not often that an end-user of materials developed for use in medicine switches roles, but I love a challenge!",
-    description3: "Currently, I map JSON to utilize FHIR resources & Python scripting in Product Operations. I also contribute to rollout strategy/planning for new feature releases. I love being able to contribute to solving problems and make comprehensive SaaS products easier to use!",
+    description3: "Currently, I work in product operations where key responsibilities include: implementation of product and technical enablement utilizing FHIR. I love being able to contribute to technology in the clinical research space that improves clinical trial workflow, data integrity & results in cancer patients getting better care!",
     image: 1
 }
