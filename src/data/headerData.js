567dc600cf447b8c7687c99d527cd67d14af6a3a
misathemeb <melissabridi@gmail.com>
2025-04-18 16:23:14 -0400

    updated CV 2025

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,10 @@
 // import resume from '../assets/pdf/resume.pdf'
-import resume3 from '../assets/pdf/CV_2023.pdf'
+import resume3 from '../assets/pdf/CV_2025.pdf'
 
 export const headerData = {
     name: 'Melissa Bridi',
     title: "Front End Developer",
     description:" I\'m a research scientist who made a career change and now work in health tech. Fun facts: I have visited 25 countries around the world and have used a gamma-ray IRL to kill tumor cells working in oncology research!",
     image: 'https://avatars.githubusercontent.com/u/91221861?s=400&u=39947d831d35f97ed31f2f24f72b5d5be932387f&v=4',
-    resumePdf: resume3
+    resumePdf: CV_2025.pdf
 }
