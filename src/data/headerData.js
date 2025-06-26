1b19efca98fbedfafd39cd245a269847aeec5623
misathemeb <melissabridi@gmail.com>
2025-06-26 09:29:05 -0400

    update CV, add link to render, update about data

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,10 @@
 // import resume from '../assets/pdf/resume.pdf'
-import resume4 from '../assets/pdf/CV_2025.pdf'
+import resume5 from '../assets/pdf/Technical_PM_MBridi.pdf'
 
 export const headerData = {
     name: 'Melissa Bridi',
-    title: "Senior Technical Solutions Associate",
-    description:" I\'m a research scientist who made a career change and now work in health tech. Fun facts: I have visited 26 countries around the world and have used a gamma-ray IRL to kill tumor cells working in oncology research!",
+    title: "Senior Technical Solutions Associate @ Flatiron Health",
+    description:" I\'m a research scientist now working in health tech. Enjoys: travel, continuous learning, music and cycling",
     image: 'https://avatars.githubusercontent.com/u/91221861?s=400&u=39947d831d35f97ed31f2f24f72b5d5be932387f&v=4',
-    resumePdf: resume4
+    resumePdf: resume5
 }
